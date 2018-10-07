7c48824da2e29d4cbf4317a9840b98c986255492
Parth Shah <parth.shah@berkeley.edu>
2018-10-07 12:51:08 -0700

    hello

---
+++ react-router-master/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import Navigation from "./components/Navigation";
+
+class App extends Component {
+  render() {
+    return (
+      <Home />
+    );
+  }
+};
+
+export default App; 
